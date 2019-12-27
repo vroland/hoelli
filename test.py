9cda0d50de89f37513dff4a540909480b85cd5b8
Titus Leistner <mail@titus-leistner.de>
2019-12-27 19:08:57 +0100

    Add offset

--- test.py
+++ test.py
@@ -1,13 +1,26 @@
 import socket
+import urllib.request
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('151.217.111.34', 1234))
+
+
+def set_offset(socket):
+    # load offset
+    offset = urllib.request.urlopen('http://hoellipixelflut.de/xy/').read()
+
+    # send offset
+    cmd = (f'OFFSET ').encode() + offset
+    print(cmd)
+    socket.send(cmd)
 
 
 def pixel(x, y, r, g, b):
     cmd = ('PX %d %d %02x%02x%02x\n' % (x, y, r, g, b)).encode()
     sock.send(cmd)
 
+
+set_offset(sock)
 
 w = 20
 h = 20
