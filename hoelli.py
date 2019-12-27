1f79d1a0e07182c4a1db6d2bc2886ed930f235ef
Titus Leistner <mail@titus-leistner.de>
2019-12-27 20:34:21 +0100

    Rename and catch all

--- hoelli.py
+++ hoelli.py
@@ -1,26 +1,20 @@
-import socket
 import random
+import socket
 import time
 import urllib.request
 
-N_SOCKS = 16
-L_CMD = 1024
-DT = 60.0
 
-
-def set_offset(sockets):
+def get_offset(px_cnt=0):
     # load offset
-    offset = urllib.request.urlopen('http://hoellipixelflut.de/xy/').read()
+    url = f'http://hoellipixelflut.de/xy/?report={px_cnt}'
+    offset = urllib.request.urlopen(url).read()
 
-    # send offset
-    cmd = (f'OFFSET ').encode() + offset
-    print(cmd)
-    for sock in sockets:
-        sock.send(cmd)
+    x, y = offset.decode().split()
+    print('New offset: ', x, y)
+    return int(x), int(y)
 
 
-def main():
-    # request and set offset
+def get_img():
     lines = urllib.request.urlopen(
         'http://hoellipixelflut.de/hoelli.csv').read()
     lines = lines.decode('utf-8').split('\n')[:-1]
@@ -32,18 +26,29 @@ def main():
     h = len(img)
     w = len(img[0])
 
+    return img, w, h
+
+
+def main():
+    DT = 20.0
+    N_SOCKS = 16
+
     # connect
     sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                for _ in range(N_SOCKS)]
     for sock in sockets:
         sock.connect(('151.217.111.34', 1234))
 
-    set_offset(sockets)
+    dx, dy = get_offset()
+    print(dx, dy)
 
-    cmd = b''
-    time0 = time.time()
-    i = 0
-    print("Let's Hölli...")
+    img, w, h = get_img()
+    print(w, h)
+
+    print('Start...')
+    time0 = 0
+    i_sock = 0
+    px_cnt = 0
     while True:
         x = random.randint(0, w - 1)
         y = random.randint(0, h - 1)
@@ -51,19 +56,21 @@ def main():
         rgb = img[y][x]
         if rgb == '000000':
             continue
-        rgb = 'ff0000'
-        cmd += f'PX {x} {y} {rgb}'.encode()
 
-        if True:
-            #print(cmd)
-            sockets[i].send(cmd)
-            cmd = b''
-            i = (i + 1) % N_SOCKS
+        cmd = f'PX {x + dx} {y + dy} {rgb}\n'.encode()
+        sockets[i_sock].send(cmd)
+        px_cnt += 1
+        i_sock = (i_sock + 1) % N_SOCKS
 
-            if time.time() - time0 > DT:
-                set_offset(sockets)
-                time0 = time.time()
+        if time.time() - time0 > DT:
+            dx, dy = get_offset(px_cnt)
+            time0 = time.time()
+            px_cnt = 0
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        try:
+            main()
+        except Exception:
+            pass
