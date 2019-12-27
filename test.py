935ecf00b5a30b85427dd850a5b15d5cb3c21eb3
Titus Leistner <mail@titus-leistner.de>
2019-12-27 18:43:08 +0100

    Remove print

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ sock.connect(('151.217.111.34', 1234))
 
 def pixel(x, y, r, g, b):
     cmd = ('PX %d %d %02x%02x%02x\n' % (x, y, r, g, b)).encode()
-    print(cmd)
     sock.send(cmd)
 
 
 w = 20
 h = 20
 
+print('Start...')
 while True:
     for x in range(w):
         for y in range(h):
