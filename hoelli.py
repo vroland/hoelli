1bf2119c3d2cb6c5db77af2d97cc93d28075ac27
Valentin Roland <valentin@vroland.de>
2019-12-29 20:43:31 +0100

    wip: async

--- hoelli.py
+++ hoelli.py
@@ -3,9 +3,13 @@ import socket
 import time
 import urllib.request
 import sys
+import selectors
+import threading
+
+sel = selectors.DefaultSelector()
 
 DT = 10.0
-MAX_SOCKS = 16
+MAX_SOCKS = 10
 
 
 def call_api(px_cnt=0):
@@ -60,66 +64,100 @@ def get_cmds(dx, dy, img):
 
     return cmds
 
+current_commands = b''
+interval_pixel_count = 0
+
+pixels_per_batch = 0
+
+sockbuf = {}
+def send(sock):
+    global interval_pixel_count
+
+    try:
+        data = sockbuf.get(sock, b'')
+        sent = sock.send(data)
+        sockbuf[sock] = data[sent:]
+        if len(data) == 0:
+            sockbuf[sock] = current_commands
+
+    except Exception as e:
+        print('An exception encountered: ',
+              type(e),  e, ' . Restarting...')
+        sel.unregister(sock)
+        sock.close()
+        connect_socket()
+        return
+
+    interval_pixel_count += pixels_per_batch
+
+def update_commands():
+    global current_commands
+    global interval_pixel_count
+    global pixels_per_batch
+
+    print ("updating image...")
+    dx, dy, url = call_api(interval_pixel_count)
+    img = load_img(url)
+    cmds = get_cmds(dx, dy, img)
+    current_commands = b''.join(cmds)
+    pixels_per_batch = len(cmds)
+    for k in sockbuf:
+        sockbuf[k] = current_commands
+
+def update_loop():
+    global interval_pixel_count
+
+    while True:
+        t1 = time.time()
+        update_commands()
+        print ("updating took", time.time() - t1, "seconds")
+        interval_pixel_count = 0
+        time.sleep(10)
+
+def connect_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        #sock.connect(('wall2.c3pixelflut.de', 1234))
+        #sock.connect(('ftp.agdsn.de', 443))
+
+        sock.connect(('151.217.118.128', 1234))
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2**18)
+        sel.register(sock, selectors.EVENT_WRITE, send)
+        sock.setblocking(0)
+        print ("connected!")
+    except ConnectionRefusedError:
+        return None
+    return sock
 
 def main():
+    global interval_pixel_count
+
     # connect
     print('Connecting to the C3 Pixel Flut wall...', end='', flush=True)
-    sockets = []
     for _ in range(MAX_SOCKS):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            sock.connect(('151.217.111.34', 1234))
-        except ConnectionRefusedError:
-            break
-        sockets.append(sock)
+        connect_socket()
 
-    if len(sockets) < 1:
+    if len(sel.get_map()) < 1:
         raise ConnectionRefusedError('Could not connect with any socket.')
 
-    print(' Connected with {} sockets.'.format(len(sockets)))
+    print(' Connected with {} sockets.'.format(len(sel.get_map())))
 
-    dx, dy, url = call_api()
-    img = load_img(url)
-
-    cmds = get_cmds(dx, dy, img)
-    cmd_string = b''.join(cmds)
+    update_commands()
 
     print('Let\'s Hölli...')
 
-    time0 = time.time()
-    i_sock = 0
-    px_cnt = 0
 
     while True:
-        sockets[i_sock].send(cmd_string)
-        px_cnt += len(cmds)
-        i_sock = (i_sock + 1) % len(sockets)
-
-        if time.time() - time0 > DT:
-            ndx, ndy, nurl = call_api(px_cnt)
-
-            if nurl != url:
-                url = nurl
-                img = load_img(url)
-                cmds = get_cmds(dx, dy, img)
-
-            if ndx != dx or ndy != dy:
-                dx, dy = ndx, ndy
-                cmds = get_cmds(dx, dy, img)
-
-            time0 = time.time()
-            px_cnt = 0
-            cmd_string = b''.join(cmds)
-
+        events = sel.select()
+        for key, mask in events:
+            send(key.fileobj)
 
 if __name__ == '__main__':
     print('USAGE: python3 hoelli.py [MAX_SOCKETS]')
     if len(sys.argv) > 1:
         MAX_SOCKS = int(sys.argv[1])
 
-    while True:
-        try:
-            main()
-        except Exception as e:
-            print('An exception encountered: ',
-                  type(e),  e, ' . Restarting...')
+    update_thread = threading.Thread(target=update_loop)
+    update_thread.start()
+
+    main()
